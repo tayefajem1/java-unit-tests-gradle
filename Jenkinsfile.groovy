7bd2e7a82a4808f188f277f8cea98de5f8aa207d
HandsOnDevOpsTraining <129347802+HandsOnDevOpsTraining@users.noreply.github.com>
2025-03-13 23:24:58 +1100

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,46 @@
+pipeline {
+    agent any
+    
+    environment {
+        IMAGE_NAME = 'my-java-app'
+        IMAGE_TAG = 'latest'
+        DOCKER_REGISTRY = 'my-docker-registry'  // Change as needed
+    }
+    
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git 'https://github.com/my-repo/my-java-app.git' // Change to your repo
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                sh './gradlew clean build'
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                sh './gradlew test'
+            }
+        }
+        
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh "docker build -t $IMAGE_NAME:$IMAGE_TAG ."
+                }
+            }
+        }
+        
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    sh "docker tag $IMAGE_NAME:$IMAGE_TAG $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
+                    sh "docker push $DOCKER_REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
+                }
+            }
+        }
+    }
+}
